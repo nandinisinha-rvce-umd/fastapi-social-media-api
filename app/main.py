8b5df4a0c22aad26efda161f34077092d17a5e9d
Nandini Sinha <nandinisinha.rvce@gmail.com>
2026-07-14 01:59:01 -0400

    requirements.txt

--- app/main.py
+++ app/main.py
@@ -6,6 +6,14 @@ from .routers import post, user, auth, vote
 models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(post.router)
 app.include_router(user.router)        
 app.include_router(auth.router)
--- app/models.py
+++ app/models.py
@@ -25,7 +25,7 @@ class User(Base):
     password = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), 
                         nullable=False, server_default=text('now()'))
-    phone_number = Column(String, nullable=True)
+    #phone_number = Column(String, nullable=True)
     
 class Vote(Base):
     __tablename__ = "votes"
